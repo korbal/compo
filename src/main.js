2d9e913d076a372700f141604183903d1ef66499
Balint <balint.j.korosi@gmail.com>
2021-01-25 11:06:11 +0100

    removed vue-analytics, didn't work

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import router from './router'
 import VueAnalytics from 'vue-analytics';
 
-// Configuration VueAnalytics
-Vue.use(VueAnalytics, {
-  id: 'UA-3450053-63',
-  router
-});
+// // Configuration VueAnalytics
+// Vue.use(VueAnalytics, {
+//   id: 'UA-3450053-63',
+//   router
+// });
 
 createApp(App).use(router).mount('#app')
