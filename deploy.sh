1eb51776e1ba31564ba67f7d6e45c7381ab78c1a
korbal <balint.j.korosi@gmail.com>
2022-08-08 17:03:29 +0200

    it's alive!

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,12 @@ cd dist
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "deploy"
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:korbal/compo.git master:gh-pages
+git push -f git@github.com:korbal/compo.git main:gh-pages
 
 cd -
